54ecb85a39c1441ff1d0135b3e8e63b321163b3d
Edouard Penin <e.peninb@gmail.com>
2019-03-30 16:19:45 +0100

    Extracted Messages component

--- src/Channel.tsx
+++ src/Channel.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { ChatInputBox } from "./ChatInputBox"
+import { Messages } from "./Messages"
 
 
 
@@ -23,31 +24,9 @@ export function Channel( {}: ChannelProps )
 					</div>
 					<div className="ChannelName">#general</div>
 				</div>
-				<div className="Messages">
-					<div className="EndOfMessages">That's every message!</div>
-					<div>
-						<div className="Day">
-							<div className="DayLine"/>
-							<div className="DayText">12/6/2018</div>
-							<div className="DayLine"/>
-						</div>
-						<div className="Message with-avatar">
-							<div className="Avatar"/>
-							<div className="Author">
-								<div>
-									<span className="UserName">Ryan Florence </span>
-									<span className="TimeStamp">3:37 PM</span>
-								</div>
-								<div className="MessageContent">Alright, lets do this.</div>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className="Message no-avatar">
-							<div className="MessageContent">works now?</div>
-						</div>
-					</div>
-				</div>
+				<Messages>
+				</Messages>
+				
 				<ChatInputBox/>
 			
 			</div>
---
+++ src/Messages.tsx
@@ -0,0 +1,43 @@
+import React from "react"
+
+
+
+
+export interface MessagesProps
+{
+
+}
+
+
+export function Messages( {}: MessagesProps )
+{
+	
+	return (
+		<div className="Messages">
+			<div className="EndOfMessages">That's every message!</div>
+			<div>
+				<div className="Day">
+					<div className="DayLine"/>
+					<div className="DayText">12/6/2018</div>
+					<div className="DayLine"/>
+				</div>
+				<div className="Message with-avatar">
+					<div className="Avatar"/>
+					<div className="Author">
+						<div>
+							<span className="UserName">Ryan Florence </span>
+							<span className="TimeStamp">3:37 PM</span>
+						</div>
+						<div className="MessageContent">Alright, lets do this.</div>
+					</div>
+				</div>
+			</div>
+			<div>
+				<div className="Message no-avatar">
+					<div className="MessageContent">works now?</div>
+				</div>
+			</div>
+		</div>
+	
+	)
+}
