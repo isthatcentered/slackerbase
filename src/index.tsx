e24485ba3ac1027c4eebb0bcb205745ae8e9ae9b
Edouard Penin <e.peninb@gmail.com>
2019-03-30 11:15:01 +0100

    Added firebase database config

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from "firebase"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const config = {
+	apiKey:            "AIzaSyA6knUASvHehbo1hNQmWTFmG2WlszEROTg",
+	authDomain:        "reach-slack.firebaseapp.com",
+	databaseURL:       "https://reach-slack.firebaseio.com",
+	projectId:         "reach-slack",
+	storageBucket:     "reach-slack.appspot.com",
+	messagingSenderId: "451211342052",
+}
+
+firebase.initializeApp(config);
+
+ReactDOM.render( <App/>, document.getElementById( "root" ) );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
