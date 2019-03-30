4e59256ce2412447bff245828c96b27fc209f4e6
Edouard Penin <e.peninb@gmail.com>
2019-03-30 16:13:29 +0100

    Any channel can post a new message to the "general" channel

--- src/Channel.tsx
+++ src/Channel.tsx
@@ -1,7 +1,14 @@
-import React from "react"
+import React, { FormEvent } from "react"
+import { db } from "./index"
 
 
 
+
+export interface message
+{
+	body: string
+	createdAt: Date
+}
 
 export interface ChatInputBoxProps
 {
@@ -12,13 +19,39 @@ export interface ChatInputBoxProps
 export function ChatInputBox( {}: ChatInputBoxProps )
 {
 	
+	function handleSubmit( e: FormEvent<HTMLFormElement> )
+	{
+		e.preventDefault()
+		
+		const data    = new FormData( e.target as HTMLFormElement ),
+		      message = data.get( "message" ) as string || undefined
+		
+		
+		if ( !message )
+			return;
+		
+		
+		(e.target as HTMLFormElement).reset()
+		
+		const post: message = {
+			body:      message,
+			createdAt: new Date(),
+		}
+		
+		db.collection( "/channels/general/messages" )
+			.add( post )
+	}
+	
+	
 	return (
-		<div className="ChatInputBox">
+		<form onSubmit={handleSubmit}
+		      className="ChatInputBox">
 			<input
+				name="message"
 				className="ChatInput"
 				placeholder="Message #general"
 			/>
-		</div>
+		</form>
 	)
 }
 
