9d552c9cb4c6e009483097a33ae655671d5fdf91
Edouard Penin <e.peninb@gmail.com>
2019-04-02 18:12:02 +0200

    Function catches the online and offline state of user throught fb rtdb and updates firestore with it

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,31 @@
 import * as functions from "firebase-functions"
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https
-	.onRequest( ( req, res ) => {
+
+
+const admin = require( "firebase-admin" );
+
+admin.initializeApp();
+
+export const db = admin.firestore()
+
+export const onUserStatusChanged = functions.database
+	.ref( `status/{userId}` )
+	.onUpdate( ( change, context ) => {
 		
-		res.send( "Hello from Firebase!" )
+		const eventStatus = change.after.val() as { status: "online" | "offline", lastChanged: any }
 		
+		const userDoc = db.doc( `users/${context.params.userId}` )
+		
+		return change.after.ref.once( "value" ).then( snapshot => {
+			const status = snapshot.val()
+			if ( status.lastChanged > eventStatus.lastChanged )
+				return
+			
+			eventStatus.lastChanged = new Date( eventStatus.lastChanged )
+			
+			userDoc.update( { status: eventStatus } )
+		} )
 	} )
+
+
